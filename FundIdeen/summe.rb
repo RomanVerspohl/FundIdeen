1360ea55805cd7fb0f130393ea7f23638a767f99
Roman <rverspohl@web.de>
2017-07-25 16:20:59 +0200

    summer wurde hinzugefugt

---
+++ FundIdeen/summe.rb
@@ -0,0 +1,23 @@
+Shoes.app :width => 500, :height => 500 do 
+
+stack :width => 200, :height => 200, :border => "red" do
+	border red
+	button "Button 1"
+	button "Button 2"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+	button "Button 3"
+
+end
+end
